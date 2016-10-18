839d1f5096afecc4c7b0a0d54dca0c5e6e027ae4
Alexander Hendrawan tjahja & Izhharuddin Syanmil <pair+alexanderHT.syanmil@hacktiv8.com>
2016-10-18 11:14:50 +0700

    basic routing done with little render

---
+++ myapp/src/server.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const express = require('express')
+const pug = require('pug');
+const path = require('path');
+
+
+let app = express()
+
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'pug');
+
+app.get('/', function (req, res){
+  res.render('server', {title: "I love Hacktiv8!"})
+});
+
+app.post('/', function(req, res){
+  res.send('Got a POST request');
+});
+
+app.put('/', function(req, res){
+  res.send('Got a PUT request at /user')
+});
+
+app.delete('/', function(req, res){
+  res.send('Got a Delete request at /user')
+})
+
+app.listen(3000)
